3ca078ec19fd48f2cf7459bf9c726d08483caddb
ct57401u <ct57401u@gbpuat.ac.in>
2023-01-13 10:51:41 +0530

    dark mode

--- darkMode.js
+++ darkMode.js
@@ -1,86 +1,93 @@
 document.getElementById("dark").addEventListener('click', ()=>{
-    if(document.getElementById('dark').classList.contains('dark')){
-        document.querySelector('body').style.background = 'var(--lGreyBg)';
-        document.querySelector('body').style.color = 'var(--lBlueTex)';
-        document.querySelector('.enter').style.color = 'var(--lBlueTex)';
-        document.querySelector('.enter').classList.add('your-class');
-        document.querySelector('.selected').style.color = 'var(--lBlueTex)';
-        document.querySelector('.options-container').style.color = 'var(--lBlueTex)';
-        document.querySelector('.country').style.color = 'var(--lBlueTex)';
-        document.querySelector('header').style.background = 'var(--dTexlElwhite)';
-        document.querySelector('.backk').style.background = 'var(--dTexlElwhite)';
-        document.querySelector('.options-container').style.background = 'var(--dTexlElwhite)';
-        document.getElementById('dark').classList.toggle('dark');
-        document.querySelector('.input').style.background = 'var(--dTexlElwhite)';
-        document.querySelector('.enter').style.background = 'var(--dTexlElwhite)';
-        document.querySelector('.selected').style.background = 'var(--dTexlElwhite)';
-        document.querySelectorAll('.card').forEach((el)=>{
-            el.style.background = 'var(--dTexlElwhite)';
-            el.style.color = 'var(--lBlueTex)';
-            document.querySelectorAll('.pop').forEach((ll)=>{
-                ll.style.color = 'var(--lBlueTex)';
-            })
-        })
-        document.querySelectorAll('.neigh').forEach((el)=>{
-            el.style.background = 'var(--dTexlElwhite)';
-            el.style.color = 'var(--lBlueTex)';
-        })
-        document.querySelectorAll('small').forEach((el)=>{
-            // el.style.background = 'var(--dTexlElwhite)';
-            el.style.color = 'var(--lBlueTex)';
-        })
-        document.querySelectorAll('.country').forEach((el)=>{
-            // el.style.background = 'var(--dTexlElwhite)';
-            el.style.color = 'var(--lBlueTex)';
-        })
-        // document.querySelectorAll('h6').forEach((el)=>{
-        //     // el.style.background = 'var(--dTexlElwhite)';
-        //     el.style.color = 'rgba(0, 0, 0, 0.915)';
-        //     document.querySelectorAll('.small').forEach((ll)=>{
-        //         ll.style.color = 'rgba(0, 0, 0, 0.615) !important';
-        //     })
-        // })
-        
+    document.querySelector('body').classList.toggle('dark');
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    if(rs.getPropertyValue('--dBlueEl') === 'hsl(209, 23%, 22%)'){
+        r.style.setProperty('--dBlueEl', 'hsl(0, 0%, 100%)');
+    }else{
+        r.style.setProperty('--dBlueEl', 'hsl(209, 23%, 22%)');
     }
-    else{
-        // document.querySelectorAll('h6').forEach((el)=>{
-        //     // el.style.background = 'var(--dTexlElwhite)';
-        //     el.style.color = 'rgba(255, 255, 255, 0.915)';
-        //     document.querySelectorAll('.small').forEach((ll)=>{
-        //         ll.style.color = 'rgba(255, 255, 255, 0.615) !important';
-        //     })
-        // })
-        document.querySelectorAll('.neigh').forEach((el)=>{
-            el.style.background = 'var(--dBlueEl)';
-            el.style.color = 'var(--dTexlElwhite)';
-        })
-        document.querySelectorAll('small').forEach((el)=>{
-            // el.style.background = 'var(--dBlueEl)';
-            el.style.color = 'var(--dTexlElwhite)';
-        })
-        document.querySelectorAll('.country').forEach((el)=>{
-            // el.style.background = 'var(--dBlueEl)';
-            el.style.color = 'var(--dTexlElwhite)';
-        })
-        document.querySelector('.enter').classList.remove('your-class');
-        document.querySelector('body').style.background = 'var(--dBlueBg)';
-        document.querySelector('.selected').style.color = 'var(--dTexlElwhite)';
-        document.querySelector('.options-container').style.color = 'var(--dTexlElwhite)';
-        document.getElementById('dark').classList.toggle('dark');
-        document.querySelector('.input').style.background = 'var(--dBlueEl)';
-        document.querySelector('.backk').style.background = 'var(--dBlueEl)';
-        document.querySelector('.options-container').style.background = 'var(--dBlueEl)';
-        document.querySelector('header').style.background = 'var(--dBlueEl)';
-        document.querySelector('.enter').style.background = 'var(--dBlueEl)';
-        document.querySelector('.selected').style.background = 'var(--dBlueEl)';
-        document.querySelector('body').style.color = 'var(--dTexlElwhite)';
-        document.querySelector('.enter').style.color = 'var(--dTexlElwhite)';
-        document.querySelectorAll('.card').forEach((el)=>{
-            el.style.background = 'var(--dBlueEl)';
-            el.style.color = 'var(--dTexlElwhite)';
-            document.querySelectorAll('.pop').forEach((ll)=>{
-                ll.style.color = 'rgba(255, 255, 255, 0.766)';
-            })
-        })
+
+    
+    if(rs.getPropertyValue('--dBlueBg') !== 'hsl(207, 26%, 17%)'){
+        r.style.setProperty('--dBlueBg', 'hsl(0, 0%, 90%)');
+    }else{
+        r.style.setProperty('--dBlueBg', 'hsl(207, 26%, 17%)');
+    }
+
+    
+    if(rs.getPropertyValue('--dTexlElwhite') !== 'hsl(0, 0%, 100%)'){
+        r.style.setProperty('--dTexlElwhite', 'hsl(200, 15%, 8%)');
+    }else{
+        r.style.setProperty('--dTexlElwhite', 'hsl(0, 0%, 100%)');
     }
 })
+
+// window.addEventListener('click', ()=>{
+//     if(document.querySelector('body').classList.contains('dark')){
+//         document.querySelector('body').style.background = 'var(--lGreyBg)';
+//         document.querySelector('body').style.color = 'var(--lBlueTex)';
+//         document.querySelector('.enter').style.color = 'var(--lBlueTex)';
+//         document.querySelector('.enter').classList.add('your-class');
+//         document.querySelector('.selected').style.color = 'var(--lBlueTex)';
+//         document.querySelector('.options-container').style.color = 'var(--lBlueTex)';
+//         document.querySelector('.country').style.color = 'var(--lBlueTex)';
+//         document.querySelector('header').style.background = 'var(--dTexlElwhite)';
+//         document.querySelector('.backk').style.background = 'var(--dTexlElwhite)';
+//         document.querySelector('.options-container').style.background = 'var(--dTexlElwhite)';
+//         document.getElementById('dark').classList.toggle('dark');
+//         document.querySelector('.input').style.background = 'var(--dTexlElwhite)';
+//         document.querySelector('.enter').style.background = 'var(--dTexlElwhite)';
+//         document.querySelector('.selected').style.background = 'var(--dTexlElwhite)';
+//         document.querySelectorAll('.card').forEach((el)=>{
+//             el.style.background = 'var(--dTexlElwhite)';
+//             el.style.color = 'var(--lBlueTex)';
+//             document.querySelectorAll('.pop').forEach((ll)=>{
+//                 ll.style.color = 'var(--lBlueTex)';
+//             })
+//         })
+//         document.querySelectorAll('.neigh').forEach((el)=>{
+//             el.style.background = 'var(--dTexlElwhite)';
+//             el.style.color = 'var(--lBlueTex)';
+//         })
+//         document.querySelectorAll('small').forEach((el)=>{
+//             el.style.color = 'var(--lBlueTex)';
+//         })
+//         document.querySelectorAll('.country').forEach((el)=>{
+//             el.style.color = 'var(--lBlueTex)';
+//         })
+        
+//     }
+//     else{
+//         document.querySelectorAll('.neigh').forEach((el)=>{
+//             el.style.background = 'var(--dBlueEl)';
+//             el.style.color = 'var(--dTexlElwhite)';
+//         })
+//         document.querySelectorAll('small').forEach((el)=>{
+//             el.style.color = 'var(--dTexlElwhite)';
+//         })
+//         document.querySelectorAll('.country').forEach((el)=>{
+//             el.style.color = 'var(--dTexlElwhite)';
+//         })
+//         document.querySelector('.enter').classList.remove('your-class');
+//         document.querySelector('body').style.background = 'var(--dBlueBg)';
+//         document.querySelector('.selected').style.color = 'var(--dTexlElwhite)';
+//         document.querySelector('.options-container').style.color = 'var(--dTexlElwhite)';
+//         document.getElementById('dark').classList.toggle('dark');
+//         document.querySelector('.input').style.background = 'var(--dBlueEl)';
+//         document.querySelector('.backk').style.background = 'var(--dBlueEl)';
+//         document.querySelector('.options-container').style.background = 'var(--dBlueEl)';
+//         document.querySelector('header').style.background = 'var(--dBlueEl)';
+//         document.querySelector('.enter').style.background = 'var(--dBlueEl)';
+//         document.querySelector('.selected').style.background = 'var(--dBlueEl)';
+//         document.querySelector('body').style.color = 'var(--dTexlElwhite)';
+//         document.querySelector('.enter').style.color = 'var(--dTexlElwhite)';
+//         document.querySelectorAll('.card').forEach((el)=>{
+//             el.style.background = 'var(--dBlueEl)';
+//             el.style.color = 'var(--dTexlElwhite)';
+//             document.querySelectorAll('.pop').forEach((ll)=>{
+//                 ll.style.color = 'rgba(255, 255, 255, 0.766)';
+//             })
+//         })
+//     }
+// })
--- main.js
+++ main.js
@@ -153,18 +153,20 @@ document.querySelector('.enter').addEventListener('keyup', (e)=>{
 
                     let nn = "";
                     neighb = nation.borders;
-                    neighb.forEach((acc) =>{
-                        an = "";
-                        data.forEach((nation) =>{
-                            if(acc === nation.cca3){
-                                an = nation.name.common;
-                                nn += `
-                                    <div class="neigh">${an}</div>
-                                `;
-                            }
+                    if(neighb){
+                        neighb.forEach((acc) =>{
+                            an = "";
+                            data.forEach((nation) =>{
+                                if(acc === nation.cca3){
+                                    an = nation.name.common;
+                                    nn += `
+                                        <div class="neigh">${an}</div>
+                                    `;
+                                }
+                            })
+                            
                         })
-                        
-                    })
+                    }
 
                     output += `
                         <div>
@@ -201,4 +203,107 @@ document.querySelector('.enter').addEventListener('keyup', (e)=>{
             })
         })
     }
+})
+
+
+document.querySelector('.row').addEventListener('click', (e)=>{
+    if(!e.target.classList.contains('row')){
+        let country;
+        if(e.target.classList.contains('card')){
+            country = e.target.childNodes[3].childNodes[1].innerHTML;
+        }
+        if(e.target.classList.contains('card-body')){
+            country = e.target.childNodes[1].innerHTML;
+        }
+        if(e.target.classList.contains('flag')){
+            country = e.target.nextSibling.nextSibling.childNodes[1].innerHTML;
+        }
+        if(e.target.classList.contains('country')){
+            country = e.target.innerHTML;
+        }
+        if(e.target.classList.contains('pop')){
+            country = e.target.parentElement.childNodes[1].innerHTML;
+        }
+
+
+
+        const input = country.replaceAll(' ', '').toLowerCase();
+        // console.log(input)
+        showloader();
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then((data) => {
+            hideloader();
+            let output = "";
+            data.forEach((nation)=>{
+                const country = nation.name.common.replaceAll(' ','').toLowerCase();
+                if(country === input){
+                    console.log(nation)
+                    const population = nation.population;
+                    const region = nation.region;
+                    const cap = nation.capital;
+                    const flag = nation.flags.png;
+                    const nativeName = walk(nation.name.nativeName);
+                    const sub = nation.subregion;
+                    const tld = nation.tld[0];
+                    const curren = walk2(nation.currencies);
+                    const lang = nation.languages;
+                    let lan = "";
+                    for(const key in lang){
+                        lan+= `${lang[key]} `;
+                    }
+
+                    let nn = "";
+                    neighb = nation.borders;
+                    if(neighb){
+                        neighb.forEach((acc) =>{
+                            an = "";
+                            data.forEach((nation) =>{
+                                if(acc === nation.cca3){
+                                    an = nation.name.common;
+                                    nn += `
+                                        <div class="neigh">${an}</div>
+                                    `;
+                                }
+                            })
+                            
+                        })
+                    }
+
+                    output += `
+                        <div>
+                            <img src="${flag}">
+                        </div>
+                        <div class="detail">
+                            <div><h1 style="font-size: 1.65em;">${nation.name.common}</h1></div>
+                            <div class="all">
+                                <div>
+                                    <h6 style="font-weight: 500 ;">Native Name: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${nativeName}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Population: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${population}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Region: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${region}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Subregion: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${sub}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Capital: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${cap}</small></h6>
+                                </div>
+                                <div>
+                                    <h6 style="font-weight: 500 ;">Top Level Domain: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${tld}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Currencies: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${curren}</small></h6>
+                                    <h6 style="font-weight: 500 ;">Language: <small class="small" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">${lan}</small></h6>
+                                </div>
+                            </div>
+                            <div class="border">
+                                <h6 style="font-weight: 500 ;">Border Countries:&nbsp;</h6> <span class="border" style="font-weight:200;color: rgba(255, 255, 255, 0.7)">
+                                    ${nn}
+                                </span>
+                            </div>
+                        </div>
+                    `
+                    document.querySelector('.roww').innerHTML = output;
+                    document.querySelector('.main').classList.add('hidden');
+                    document.querySelector('.container').classList.add('hidden');
+                    document.querySelector('.check').classList.remove('hidden');
+                }
+            })
+        })
+    }
+    
 })
